db81c1129e634f411bda60af972e3ab8baba9404
fabioarmbruster <fabioarmbruster@localhost>
2018-03-22 19:53:39 +0100

    Funktion Test

---
+++ Aufgabe 0/js/Aufgabe0.js
@@ -0,0 +1,8 @@
+function Namensfeld() {
+    var name = prompt("Wieso ist dein Name", "");
+    if (name != null) {
+        document.getElementById("input").innerHTML =
+            "Dein Name ist " + name + ". Wieso auch nicht?";
+    }
+}
+//# sourceMappingURL=Aufgabe0.js.map
---
+++ Aufgabe 0/js/Aufgabe0.ts
@@ -0,0 +1,7 @@
+function Namensfeld(): void {
+  var name: string = prompt("Wieso ist dein Name", "");
+  if (name != null) {
+    document.getElementById("input").innerHTML =
+      "Dein Name ist " + name + ". Wieso auch nicht?";
+  }
+}
