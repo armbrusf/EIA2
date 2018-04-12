54e17047d72db0eee6d587d8e796a189328d8a85
fabioarmbruster <fabioarmbruster@fabios-mbp.eduroam.hs-furtwangen.de>
2018-04-12 16:57:54 +0200

    a

--- Aufgabe2/js/memory.js
+++ Aufgabe2/js/memory.js
@@ -1,1 +1,13 @@
+var Aufgabe2;
+(function (Aufgabe2) {
+    //Funktion wird ausgeführt wennn DOM geladen ist.
+    window.addEventListener("DOMContentLoaded", init);
+    //init Funktion
+    function init() {
+        let cardContent = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+        let numPlayers;
+        let numPairs;
+        let n;
+    }
+})(Aufgabe2 || (Aufgabe2 = {}));
 //# sourceMappingURL=memory.js.map
---
+++ Aufgabe2/js/memory.ts
@@ -0,0 +1,44 @@
+namespace Aufgabe2 {
+    //Funktion wird ausgeführt wennn DOM geladen ist.
+    window.addEventListener("DOMContentLoaded", init);
+    
+    //init Funktion
+    function init(): void {
+        let cardContent : string[] = ["A","B","C","D","E","F","G","H","I","J"];
+        let n : number;
+        
+        let numPlayers : string = prompt("Spieleranzahl","");
+        let numPlayersFinal : number = parseInt(numPlayers);
+        
+        let numPairs : string = prompt("Kartenanzahl","");
+        let numPairsFinal : number = parseInt(numPairs);
+        
+        let cardsCloned: string = createCards(cardContent)
+        }   
+        
+    function createCards(_cardContent:string[]) : string[] {
+        let clone : string = _cardContent.slice(0);
+        for (let i : number = 0; i < clone.length; i++) {
+            _cardContent.push(clone[i])
+            }
+        }
+        
+        
+          
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
