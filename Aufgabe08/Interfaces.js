b2b05caa4190d31532b937a8d35525036e4f7768
fabioarmbruster <fabioarmbruster@fabios-mbp.hfu-open.fu.hs-furtwangen.de>
2018-06-10 17:28:28 +0200

    sdaf

---
+++ Aufgabe08/Interfaces.js
@@ -0,0 +1,6 @@
+var Aufgabe08;
+(function (Aufgabe08) {
+    // Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+    Aufgabe08.studiHomoAssoc = {};
+})(Aufgabe08 || (Aufgabe08 = {}));
+//# sourceMappingURL=Interfaces.js.map
